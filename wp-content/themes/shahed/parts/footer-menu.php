9afed5c6baa8ea16fa7f551952ad9eaba90a223f
deepFace <deepFace@SAMSUNG-PC>
2025-06-06 03:05:53 +0400

    feat: cleanup footer menu

--- wp-content/themes/shahed/parts/footer-menu.php
+++ wp-content/themes/shahed/parts/footer-menu.php
@@ -1,1 +1,61 @@
-<?php
+
+<ul class="flex flex-col flex-wrap gap-y-[24px] md:flex-row md:items-start md:justify-center md:vw-gap-x-[12] lg:vw-gap-x-[16] mx-0 mb-3 mt-0 md:vw-mb-[32] md:vw-mt-[48] leading-normal md:w-[90%] lg:w-[80%] ltr:leading-tight">
+    <li class="group relative mx-0 flex items-center justify-center text-center md:mb-0 text-[14px] md:text-md-14 lg:text-lg-14 xl:text-xl-16 2xl:text-2xl-16">
+        <a href="#">
+            <h3 class="transition-colors duration-500 hover:text-footerGreenText text-white">&nbsp;الرئيسية</h3>&nbsp;
+        </a>
+    </li>
+
+    <li class="group relative mx-0 flex items-center justify-center text-center md:mb-0 text-[14px] md:text-md-14 lg:text-lg-14 xl:text-xl-16 2xl:text-2xl-16">
+        <a href="#">
+            <h3 class="transition-colors duration-500 hover:text-footerGreenText text-white">&nbsp;اتصل بنا&nbsp;</h3>
+        </a>
+    </li>
+
+    <li class="group relative mx-0 flex items-center justify-center text-center md:mb-0 text-[14px] md:text-md-14 lg:text-lg-14 xl:text-xl-16 2xl:text-2xl-16">
+        <a href="#" target=_blank rel="noopener noreferrer nofollow">
+            <h3 class="transition-colors duration-500 hover:text-footerGreenText text-white">&nbsp;مركز خدمة العملاء&nbsp;</h3>
+        </a>
+    </li>
+
+    <li class="group relative mx-0 flex items-center justify-center text-center md:mb-0 text-[14px] md:text-md-14 lg:text-lg-14 xl:text-xl-16 2xl:text-2xl-16">
+        <a href="#">
+            <h3 class="transition-colors duration-500 hover:text-footerGreenText text-white">&nbsp;تطبيقات&nbsp;</h3></a>
+    </li>
+
+    <li>
+        <div class="cursor-pointer text-white transition-colors duration-500 hover:text-footerGreenText">إدارة ملفات تعريف الارتباط</div>
+    </li>
+
+
+    <li class="group relative mx-0 flex items-center justify-center text-center md:mb-0 text-[14px] md:text-md-14 lg:text-lg-14 xl:text-xl-16 2xl:text-2xl-16">
+        <a href="#">
+            <h3 class="transition-colors duration-500 hover:text-footerGreenText text-white">&nbsp;الشروط والأحكام&nbsp;</h3>
+        </a>
+    </li>
+
+    <li class="group relative mx-0 flex items-center justify-center text-center md:mb-0 text-[14px] md:text-md-14 lg:text-lg-14 xl:text-xl-16 2xl:text-2xl-16">
+        <a href="#">
+            <h3 class="transition-colors duration-500 hover:text-footerGreenText text-white">&nbsp;سياسة الخصوصية&nbsp;</h3>
+        </a>
+    </li>
+
+    <li class="group relative mx-0 flex items-center justify-center text-center md:mb-0 text-[14px] md:text-md-14 lg:text-lg-14 xl:text-xl-16 2xl:text-2xl-16">
+        <img alt="مركز خدمة العملاء" loading="lazy" width="16" height="16" decoding="async" data-nimg="1" class="mr-[1.25vw] w-3.5 md:vw-mr-[4] 2xl:vw-h-[14] 2xl:vw-w-[14]" src="<?php _e_(\Shahed\Assets::toWpAssetSrc('/images/icons/phone.svg')); ?>" style="color:transparent">
+
+        <a class="cursor-pointer"><h3 class="transition-colors duration-500 hover:text-footerGreenText text-white cursor-pointer">&nbsp;مركز خدمة العملاء&nbsp;</h3></a>
+
+        <div class="absolute top-0 z-10 inline-block w-max px-[10px] py-[10px] rounded-lg border border-transparent bg-white text-left leading-tight md:left-[50%] md:translate-x-[-50%] lg:rounded-lg-4 lg:p-lg-16 lg:text-lg-14 2xl:rounded-2xl-4 2xl:p-2xl-16 2xl:text-2xl-14 transition-all duration-300 ease-in-out invisible translate-y-[-100%] opacity-0 group-hover:translate-y-[-110%] group-hover:md:visible group-hover:md:opacity-100">
+            <div class="whitespace-pre-line font-sansBold font-semibold text-customerCarePopoverTitle lg:mb-lg-12 2xl:mb-2xl-12">هل تحتاج للمساعدة بخصوص اشتراكك؟
+                اتصل على الأرقام التالية:
+            </div>
+            <div>
+                <div class="pb-[5vw] text-footerGrayText md:pb-0"><span class="flex pb-[1.25vw] md:inline-block md:pb-0 lg:mr-lg-12 2xl:mr-2xl-12">المملكة العربية السعودية:</span><a href="tel:+966 115 101 940" class="font-shahidMedium text-customerCarePopoverTitle hover:text-footerGreenText max-md:text-[18px]">+966 115 101 940</a></div>
+                <div class="pb-[5vw] text-footerGrayText md:pb-0"><span class="flex pb-[1.25vw] md:inline-block md:pb-0 lg:mr-lg-12 2xl:mr-2xl-12">الإمارات العربية المتحدة: </span><a href="tel:04 5677 400" class="font-shahidMedium text-customerCarePopoverTitle hover:text-footerGreenText max-md:text-[18px]">04 5677 400</a></div>
+                <div class="pb-[5vw] text-footerGrayText md:pb-0"><span class="flex pb-[1.25vw] md:inline-block md:pb-0 lg:mr-lg-12 2xl:mr-2xl-12">المملكة الأردنية الهاشمية: </span><a href="tel:06 5777 577" class="font-shahidMedium text-customerCarePopoverTitle hover:text-footerGreenText max-md:text-[18px]">06 5777 577</a></div>
+                <div class="pb-[5vw] text-footerGrayText md:pb-0"><span class="flex pb-[1.25vw] md:inline-block md:pb-0 lg:mr-lg-12 2xl:mr-2xl-12">الرقم الدولي: </span><a href="tel:+966 115 101 940" class="font-shahidMedium text-customerCarePopoverTitle hover:text-footerGreenText max-md:text-[18px]">+966 115 101 940</a></div>
+            </div>
+            <div class="absolute bottom-0 left-2/4 h-2 w-2 after:absolute after:left-1/2 after:top-[100%] after:-translate-x-1/2 after:border-8 after:border-white after:border-x-transparent after:border-b-transparent after:content-['']"></div>
+        </div>
+    </li>
+</ul>
