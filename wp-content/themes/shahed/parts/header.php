59dfd18618bee73b6870d6006bdd9d03fa361151
deepFace <deepFace@SAMSUNG-PC>
2025-06-08 22:43:57 +0400

    fear: change images

--- wp-content/themes/shahed/parts/header.php
+++ wp-content/themes/shahed/parts/header.php
@@ -2,7 +2,7 @@
     <header class="font-reset fixed m-auto flex h-12 flex-col justify-center text-base leading-[19px] lg:flex-row left-0 right-0 z-[1120] w-full max-w-full md:h-md-48 xl:h-16 2xl:h-2xl-64 md:text-md-14 lg:text-lg-14 xl:text-xl-14 2xl:text-2xl-16 md:leading-md-17 lg:leading-lg-17 xl:leading-xl-17 2xl:leading-2xl-19 md:px-md-6 lg:px-lg-10 xl:px-[42px] 2xl:px-2xl-42 text-white bg-darkBg" id="shahid-header" data-transparent-header="false">
         <div class=" flex w-full max-w-full flex-nowrap items-center justify-center lg:justify-start">
             <a  href="/" class="flex flex-none items-center justify-center m-0  cursor-pointer md:ml-md-16 lg:ml-lg-24xl:ml-6 2xl:ml-2xl-24">
-                <img alt="Shahid VIP" title="Shahid VIP" fetchpriority="high"  decoding="async" class="h-[20px] w-auto leading-[0] md:h-md-22 lg:h-lg-24 xl:h-6 2xl:h-2xl-24" src="<?php _e_(\Shahed\Assets::toWpAssetSrc('/images/logo.webp')); ?> " style="color:transparent">
+                <img alt="Shahid VIP" title="Shahid VIP" fetchpriority="high"  decoding="async" class="h-[20px] w-auto leading-[0] md:h-md-22 lg:h-lg-24 xl:h-6 2xl:h-2xl-24" src="<?php _e_(\Shahed\Assets::toWpAssetSrc('/images/logo.png')); ?> " style="color:transparent">
             </a>
         </div>
     </header>
